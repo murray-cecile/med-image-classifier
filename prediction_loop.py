44875fe1eb28dc4402a98e5e16f1cd842332582f
tamara-glazer <tamaraglazer@gmail.com>
2020-03-10 22:07:14 -0500

    git pull

--- prediction_loop.py
+++ prediction_loop.py
@@ -1,5 +1,10 @@
 '''
-Prediction Challenge Loop
+Final Script: Prediction Challenge Loop
+
+Runs the full classification loop. Inputs include training and testing set
+directory paths, as well as training and testing labels saved in a CSV.
+The script will print a summary of all evaluated models as well as the
+highest AUC acheived.
 '''
 import pandas as pd
 import sklearn
@@ -7,10 +12,8 @@ import datetime
 import timeit
 import argparse
 
-
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import ParameterGrid
-
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import Ridge, Lasso
 from sklearn.svm import SVC
@@ -20,22 +23,27 @@ from sklearn.exceptions import ConvergenceWarning
 
 import pipeline as pipe
 
-PATH = r'/Users/Tammy/Documents/_MSCAPP/Winter_2020/Computer_Vision_MP/med-image-classifier/raw_train/'
-TRAIN_CSV = r'/Users/Tammy/Documents/_MSCAPP/Winter_2020/Computer_Vision_MP/med-image-classifier/metadata/training_labels.csv'
-
 
 @ignore_warnings(category=ConvergenceWarning)
 @ignore_warnings(category=FutureWarning)
 def find_best_model(models, parameters_grid, x_train, outcome_label):
     '''
-    Cross-validation to find the best model, given parameters
+    Cross-validation to find the best model, given parameters.
+
+    Input:
+        models (dict): dictionary initializing each scikit-learn model
+        parameters_grid (dict): dictionary setting hyperparameter values
+        x_train (df): processed training feature dataframe
+        outcome_label (str): name of outcome label
+
+    Output:
+        best_model (obj): best model fit on full training data
     '''
     results_df =  pd.DataFrame(columns=('model_name',
                                         'parameters',
                                         'auc',
                                         'time_to_run'))
     max_auc = 0
-    best_model = ""
     best_model = ""
     best_parameter = ""
     start_time = timeit.default_timer()
@@ -44,23 +52,21 @@ def find_best_model(models, parameters_grid, x_train, outcome_label):
         print("Starting " + model_key + " at " + str(datetime.datetime.now()))
         model = models[model_key]
         parameter_values = parameters_grid[model_key]
+        
         for parameter in ParameterGrid(parameter_values):
             s = timeit.default_timer()
             model.set_params(**parameter)
 
-            # Calculate MSE using 5-fold cross validation
-            # Change signs because scoring is negative MSE
+            # Calculate AUC using 5-fold cross validation
             x_train_no_id = x_train.drop('id', axis=1)
             scores = cross_val_score(estimator=model,
                                      X=x_train_no_id.drop(outcome_label, axis=1),
-                                     y=x_train[outcome_label], # series or dataframe preferred?
+                                     y=x_train[outcome_label],
                                      cv=5,
-                                     scoring='roc_auc') #'neg_mean_squared_error'
-
+                                     scoring='roc_auc')
             auc = scores.mean()
             time = timeit.default_timer() - start_time
-            results_df.loc[(len(results_df))] = [model_key, parameter,
-                                                 auc, time]
+            results_df.loc[len(results_df)] = [model_key, parameter, auc, time]
 
             # Update "winner"
             if (auc > max_auc):
@@ -71,15 +77,13 @@ def find_best_model(models, parameters_grid, x_train, outcome_label):
 
     elapsed = timeit.default_timer() - start_time
 
-
     print(results_df)
     print("Highest AUC " + str(max_auc))
     print("Best Model " + str(best_model))
     print("Best Parameter " + str(best_parameter))
     print('Total Time: ', elapsed)
     
-
-    # Fit best model and best parameter on full training dataset
+    # Fit best model & best parameters on full training dataset
     best_model.set_params(**best_parameter)
     best_model.fit(x_train_no_id.drop(outcome_label, axis=1),
                    x_train[outcome_label])
@@ -89,11 +93,14 @@ def find_best_model(models, parameters_grid, x_train, outcome_label):
 
 def main(args):
     '''
-    EXECUTE FULL LOOP.
-    Takes: dictionary of command line arguments (should fix defaults later?)
-    Returns: right now, the training data, but eventually ?
-    '''
+    Full loop execution.
 
+    Input:
+        args (dict): dictionary of command line arguments
+
+    Return:
+        train (df): pre-processed training dataframe [CHANGE LATER]
+    '''
     models = {'Tree': DecisionTreeRegressor(max_depth=10),
               'Lasso': Lasso(alpha=0.1),
               'Ridge': Ridge(alpha=.5),
@@ -130,9 +137,9 @@ def main(args):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-train", "--train", default = PATH, help = "Training image file path")
-    parser.add_argument("-train_csv", "--train_csv", default = TRAIN_CSV, help = "Training csv file path")
-    parser.add_argument("-test", "--test", default="", help = "Test image file path")
+    parser.add_argument("-train", "--train", default = "", help = "Training image file path")
+    parser.add_argument("-train_csv", "--train_csv", default = "", help = "Training csv file path")
+    parser.add_argument("-test", "--test", default= "", help = "Test image file path")
     parser.add_argument("-test_csv", "--test_csv", default="", help = "Testing csv file path")
 
     args = parser.parse_args()
